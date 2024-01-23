65c1e80f9bfdc61b961520ad51bbc09e278329cc
Linh Pham <phamviethonglinh@gmail.com>
2024-01-23 20:15:30 +0700

    useEffect

--- vietpro_store/src/components/Description.js
+++ vietpro_store/src/components/Description.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const initDescription =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
@@ -6,6 +6,10 @@ const initDescription =
 const Description = () => {
   const [description, setDescription] = useState(initDescription);
   const [isFocus, setIsFocus] = useState(false);
+
+  useEffect(() => {
+    !isFocus && description === "" && setDescription(initDescription);
+  }, [description, isFocus]);
 
   return isFocus ? (
     <textarea
@@ -15,6 +19,7 @@ const Description = () => {
       onBlur={() => setIsFocus(false)}
       value={description}
       autoFocus={isFocus}
+      onKeyUp={(e) => e.key === "Enter" && setIsFocus(false)}
     >
       {description}
     </textarea>
--- vietpro_store/src/components/Title.js
+++ vietpro_store/src/components/Title.js
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const initTitle = "What is Lorem Ipsum?";
 
 const Title = () => {
-  const [title, setTitle] = useState("What is Lorem Ipsum?");
+  const [title, setTitle] = useState(initTitle);
   const [isFocus, setIsFocus] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    !isFocus && title === "" && setTitle(initTitle);
+  }, [title, isFocus]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) return <h4>Loading...</h4>;
 
   return isFocus ? (
     <input
@@ -12,6 +27,7 @@ const Title = () => {
       onChange={(e) => setTitle(e.target.value)}
       autoFocus={isFocus}
       style={{ fontWeight: "bold" }}
+      onKeyUp={(e) => e.key === "Enter" && setIsFocus(false)}
     />
   ) : (
     <h4 onDoubleClick={() => setIsFocus(true)}>{title}</h4>
